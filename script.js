3488444bea57000b8328daf582f7ba0d73a43425
devhect <48111090+devhect@users.noreply.github.com>
2019-07-09 20:41:30 -0600

    Adding initial script.js functions

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function msg(){  
-    alert("Hello World");  
-   }  
+let nSquares = 16;
+const containerWidth = 500;
+const containerHeight = 500;
+
+function createGrid(nSquares){
+    
+}
+
+// resetGridButton.addEventListener("click", reset);
+// resizeGridButton.addEventListener("click", resize);
+
+function reset(e){
+    
+}
+
+function resize(e){
+    
+}
